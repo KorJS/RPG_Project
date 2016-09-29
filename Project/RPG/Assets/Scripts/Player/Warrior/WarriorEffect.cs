0e50531d4f8b2bdd2c9f7ca917208aa4f9ba4e4e
JinSuk Lee <4316510@naver.com>
2016-09-29 17:45:01 +0900

    이펙트

--- Project/RPG/Assets/Scripts/Player/Warrior/WarriorEffect.cs
+++ Project/RPG/Assets/Scripts/Player/Warrior/WarriorEffect.cs
@@ -9,6 +9,11 @@ public class WarriorEffect : MonoBehaviour
     [System.Serializable]
     public class Effectsettings
     {
+        public GameObject blockObj;
+        public GameObject blockDamageObj;
+        public GameObject overpowerObj;
+        public GameObject rushObj;
+
         public string block = "BlockEffect";
         public string blockDamage = "BlockDamageEffect";
         public string overpower = "OverpowerEffect";
@@ -17,11 +22,25 @@ public class WarriorEffect : MonoBehaviour
 
     public Effectsettings effectSettings;
 
-    public readonly string effectPath = "Effect/Warrior";
+    private Transform skillHolder = null;
+
+    public string effectPath = "Effect/Warrior";
 
     void Awake()
     {
         warrior = GetComponent<Warrior>();
+        skillHolder = transform.FindChild("SkillHolder");
+
+        ResourceLoad();
+    }
+
+    // 이펙트 리소스 로드
+    private void ResourceLoad()
+    {
+        effectSettings.blockObj = Resources.Load(effectPath + effectSettings.block) as GameObject;
+        effectSettings.blockDamageObj = Resources.Load(effectPath + effectSettings.blockDamage) as GameObject;
+        effectSettings.overpowerObj = Resources.Load(effectPath + effectSettings.overpower) as GameObject;
+        effectSettings.rushObj = Resources.Load(effectPath + effectSettings.rush) as GameObject;
     }
 
     // TODO : 이펙트 이름으로 이펙트 생성, 위치 설정 ( 각 이펙트 프리펩에 지속시간 설정 )
@@ -34,8 +53,6 @@ public class WarriorEffect : MonoBehaviour
             return;
         }
 
-        GameObject effect = Resources.Load(effectPath + effectSettings.block) as GameObject;
-
     }
 
     // 방패막기중에 맞았을때 효과
@@ -45,8 +62,6 @@ public class WarriorEffect : MonoBehaviour
         {
             return;
         }
-
-        GameObject effect = Resources.Load(effectPath + effectSettings.blockDamage) as GameObject;
 
     }
 }
